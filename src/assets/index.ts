2652f1ee0c7df67cc61661e222df14376fb9f7d6
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-01-20 04:40:52 +0100

    💄 style: update assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,9 +3,9 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import photo from "./photo.png";
-import front from "./front.png";
-import server from "./server.png";
-import twitch from "./twitch.png";
+import front from "./computer.svg";
+import server from "./server.svg";
+import twitch from "./twitch.svg";
 
 import alt from "./experience/alt-logo.jpg";
 import pollen from "./experience/pollen-logo.jpg";
